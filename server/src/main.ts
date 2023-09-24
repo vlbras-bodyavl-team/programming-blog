d1f1e20cb5ce9c3182e6d062f4dbbf98e3e36cd8
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-09-24 12:45:48 +0300

    fix: set cors to true

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
-    }),
+    })
   );
   await app.listen(process.env.PORT);
-  Logger.log(`😎 Application is running on port ${process.env.PORT}`, `bootstrap`);
+  Logger.log(
+    `😎 Application is running on port ${process.env.PORT}`,
+    `bootstrap`
+  );
 }
 bootstrap();
