fcd172894aaca22491ce326e29f795926a7f926c
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-09-25 14:36:23 +0300

    feat: side drawer topics and posts

--- client/src/Widget/SideDrawer/SideDrawer.tsx
+++ client/src/Widget/SideDrawer/SideDrawer.tsx
@@ -1,5 +1,32 @@
 import s from "./SideDrawer.module.scss";
 import { useAppSelector } from "../../store/store";
+import Topic from "../../Components/UI/Topic/Topic";
+import { ITopic } from "../../interfaces";
+
+const mockTopic: ITopic = {
+  id: "id",
+  name: "Topic",
+  posts: [
+    {
+      id: "id",
+      title: "test",
+      content: "test",
+      topic: "id",
+    },
+    {
+      id: "id",
+      title: "test",
+      content: "test",
+      topic: "id",
+    },
+    {
+      id: "id",
+      title: "test",
+      content: "test",
+      topic: "id",
+    },
+  ],
+};
 
 const SideDrawer = () => {
   const isOpen = useAppSelector((state) => state.isOpenDrawer.value);
@@ -7,7 +34,10 @@ const SideDrawer = () => {
   return (
     <div className={isOpen ? `${s.drawer} ${s.open}` : s.drawer}>
       <div className={s.content}>
-        <div>Here comes the contetnt</div>
+        <Topic topic={mockTopic} />
+        <Topic topic={mockTopic} />
+        <Topic topic={mockTopic} />
+        <Topic topic={mockTopic} />
       </div>
     </div>
   );
