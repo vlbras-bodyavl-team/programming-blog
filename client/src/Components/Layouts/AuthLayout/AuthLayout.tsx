1f28ed9f0ef7a9ec048f958a64b11c2e5f61b904
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-09-27 19:49:21 +0300

    feat: auto color theme on first load

--- client/src/Components/Layouts/AuthLayout/AuthLayout.tsx
+++ client/src/Components/Layouts/AuthLayout/AuthLayout.tsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useAppSelector } from "../../../store/store";
-import { setColorTheme } from "../../../utils";
+import { useAppDispatch } from "../../../store/store";
+import { useMediaQuery } from "react-responsive";
+import { useEffect } from "react";
+import { getColorTheme, setColorTheme } from "../../../utils";
+import { setTheme } from "../../../store/features/isDarkModeSlice";
 
 const AuthLayout = () => {
-  const isDarkMode = useAppSelector((state) => state.theme.isDarkMode);
-  setColorTheme(isDarkMode ? "dark" : "light");
+  const dispatch = useAppDispatch();
+  const systemPrefersDark = useMediaQuery({
+    query: "(prefers-color-scheme: dark)",
+  });
+  useEffect(() => {
+    if (!getColorTheme()) {
+      const currentColorSceme = systemPrefersDark ? "dark" : "light";
+      setColorTheme(currentColorSceme);
+      dispatch(setTheme(currentColorSceme));
+    } else {
+      setColorTheme(getColorTheme() === "dark" ? "dark" : "light");
+    }
+  }, []);
   return <Outlet />;
 };
 
