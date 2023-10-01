2a90a69ff6ccf40271aafa865b0c1a588578090d
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-10-02 00:56:12 +0300

    fix: consider linebraks at post content

--- server/src/posts/posts.service.ts
+++ server/src/posts/posts.service.ts
@@ -1,12 +1,12 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Post } from './entities/post.entity';
-import { Repository } from 'typeorm/repository/Repository';
-import { TopicsService } from 'src/topics/topics.service';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Post } from "./entities/post.entity";
+import { Repository } from "typeorm/repository/Repository";
+import { TopicsService } from "src/topics/topics.service";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Injectable()
 export class PostsService {
@@ -14,7 +14,7 @@ export class PostsService {
     @InjectRepository(Post)
     private postsRepository: Repository<Post>,
     private readonly topicsService: TopicsService,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) {}
 
   async create(createPostDto: CreatePostDto): Promise<Post> {
@@ -22,7 +22,7 @@ export class PostsService {
     const post = this.postsRepository.create({ ...createPostDto, topic });
 
     await this.cacheManager.del(`posts-${topic.id}`);
-    
+
     return this.postsRepository.save(post);
   }
 
@@ -42,15 +42,21 @@ export class PostsService {
 
   async findOne(id: string): Promise<Post> {
     const post = await this.postsRepository
-      .createQueryBuilder('post')
-      .leftJoin('post.topic', 'topic')
+      .createQueryBuilder("post")
+      .leftJoin("post.topic", "topic")
 
-      .select(['post.id', 'post.title', 'post.content', 'topic.id', 'topic.name'])
-      .where('post.id = :id', { id })
+      .select([
+        "post.id",
+        "post.title",
+        "post.content",
+        "topic.id",
+        "topic.name",
+      ])
+      .where("post.id = :id", { id })
       .getOne();
 
     if (!post) {
-      throw new NotFoundException('Post not found');
+      throw new NotFoundException("Post not found");
     }
 
     return post;
@@ -59,9 +65,9 @@ export class PostsService {
   async update(id: string, updatePostDto: UpdatePostDto): Promise<Post> {
     const topic = await this.topicsService.preload(updatePostDto.topic);
     const post = await this.findOne(id);
-    
+
     await this.cacheManager.del(`posts-${topic.id}`);
-    
+
     this.postsRepository.merge(post, { ...updatePostDto, topic });
     return this.postsRepository.save(post);
   }
@@ -70,7 +76,7 @@ export class PostsService {
     const post = await this.findOne(id);
 
     await this.cacheManager.del(`posts-${post.topic.id}`);
-    
+
     return this.postsRepository.remove(post);
   }
 }
