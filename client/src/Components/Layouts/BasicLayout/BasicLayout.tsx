013851c66082c5d577cb4315a2b3cfe6fa642d36
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-10-06 13:37:01 +0300

    feat: scroll for drawer, logout button on mobile

--- client/src/Components/Layouts/BasicLayout/BasicLayout.tsx
+++ client/src/Components/Layouts/BasicLayout/BasicLayout.tsx
@@ -2,14 +2,17 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../../../Widget";
 import s from "./BasicLayout.module.scss";
 import { SideDrawer } from "../../../Widget";
+import { useAppSelector } from "../../../store/store";
 
 const BasicLayout = () => {
+  const isOpenDrawer = useAppSelector((state) => state.isOpenDrawer.value);
+
   return (
     <>
       <Header />
       <div className={s.container}>
         <SideDrawer />
-        <div className={s.outlet}>
+        <div className={isOpenDrawer ? `${s.outlet} ${s.open}` : s.outlet}>
           <Outlet />
         </div>
       </div>
--- client/src/Widget/SideDrawer/SideDrawer.tsx
+++ client/src/Widget/SideDrawer/SideDrawer.tsx
@@ -3,26 +3,59 @@ import { useAppDispatch, useAppSelector } from "../../store/store";
 import Topic from "../../Components/UI/Topic/Topic";
 import { useMediaQuery } from "react-responsive";
 import { toggle } from "../../store/features/isOpenDrawerSlice";
+import axios from "axios";
+import { logout } from "../../services";
+import { removeTokensFromStorage } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
 const SideDrawer = () => {
   const isOpen = useAppSelector((state) => state.isOpenDrawer.value);
   const isDarkMode = useAppSelector((state) => state.theme.isDarkMode);
   const topics = useAppSelector((state) => state.topics.topics);
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const isTablet = useMediaQuery({ query: "screen and (max-width: 768px)" });
+  const isTablet = useMediaQuery({ query: "screen and (max-width: 767px)" });
 
   const handlePostClick = () => {
     if (isTablet) dispatch(toggle());
   };
 
+  const handleLogoutClick = async () => {
+    try {
+      await logout();
+      removeTokensFromStorage();
+      navigate("/signin");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        navigate("/signin");
+      }
+    }
+  };
+
   return (
     <div className={isOpen ? `${s.drawer} ${s.open}` : s.drawer}>
-      <ul className={isDarkMode ? `${s.content} ${s.dark}` : s.content}>
-        {topics.map((topic, i) => (
-          <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
-        ))}
-      </ul>
+      <div className={isDarkMode ? `${s.content} ${s.dark}` : s.content}>
+        <ul>
+          {topics.map((topic, i) => (
+            <>
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+              <Topic key={i} topic={topic} handlePostClick={handlePostClick} />
+            </>
+          ))}
+        </ul>
+        <h4 onClick={handleLogoutClick} className={s.logout}>
+          Logout
+        </h4>
+      </div>
     </div>
   );
 };
