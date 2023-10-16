ad4a6ed7e2094398e1de63c7d0c39b2033539678
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-10-16 21:43:00 +0300

    feat: users table

--- client/src/Pages/Users/Users.tsx
+++ client/src/Pages/Users/Users.tsx
@@ -1,7 +1,27 @@
+import { LoaderFunction, useLoaderData } from "react-router-dom";
+import { UsersTable } from "../../Widget";
 import s from "./Users.module.scss";
+import { catchUnauthorizedError } from "../../utils/router";
+import { getUsers } from "../../services";
+import { IUser } from "../../interfaces";
 
 const Users = () => {
-  return <div className={s.container}></div>;
+  const users = useLoaderData() as IUser[];
+
+  return (
+    <div className={s.container}>
+      <UsersTable users={users} />
+    </div>
+  );
+};
+
+export const usersLoader: LoaderFunction = async () => {
+  try {
+    const users = await getUsers();
+    return users;
+  } catch (error) {
+    return catchUnauthorizedError(error);
+  }
 };
 
 export default Users;
